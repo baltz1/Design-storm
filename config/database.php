3f2e7ee9707f1c0c3fa1a50f27ce703f7d4e3302
baltz <baltznunez@gmail.com>
2018-05-16 07:25:03 -0700

    added info to db

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'heroku_d4fb5d6ad9e66ef'),
             'username' => env('DB_USERNAME', 'bdafa49845e125'),
